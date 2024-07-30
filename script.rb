9b9c726acd3255935af7a01f8cbe32b375aa50b8
KaberaSamuel <kaberanshutis@gmail.com>
2024-07-31 01:24:45 +0200

    refactoring the code

--- script.rb
+++ script.rb
@@ -1,42 +1,44 @@
-
+# frozen_string_literal: true
 
 def cipher(string, shift)
-  characters = string.split("")
+  characters = string.split('')
   new_characters = []
-  alpha_hash = generate_alphabets_hashes()
+  alpha_hash = generate_alphabets_hashes
 
-  for char in characters
+  characters.each do |char|
     if alpha_hash.keys.include?(char.downcase)
       position = alpha_hash[char.downcase] + shift
-      new_char = position <= 26 ?  alpha_hash.key(position) : alpha_hash.key(position - 26)
+      new_char = position <= 26 ? alpha_hash.key(position) : alpha_hash.key(position - 26)
       new_char = char.is_lower? ? new_char : new_char.upcase
 
-    else 
+    else
       new_char = char
     end
     new_characters.push(new_char)
   end
 
-  new_characters.join("")
+  new_characters.join('')
 end
-
 
 class String
   def is_upper?
-    self == self.upcase
+    self == upcase
   end
 
   def is_lower?
-    self == self.downcase
+    self == downcase
   end
 end
 
-def generate_alphabets_hashes()
-    alphabets = "abcdefghijklmnopqrstuvwxyz".split("")
-    alphabets_hash = {}
-    counter = 1
-    alphabets.each {|character| alphabets_hash[character] = counter; counter += 1}
-   alphabets_hash
+def generate_alphabets_hashes
+  alphabets = 'abcdefghijklmnopqrstuvwxyz'.split('')
+  alphabets_hash = {}
+  counter = 1
+  alphabets.each do |character|
+    alphabets_hash[character] = counter
+    counter += 1
+  end
+  alphabets_hash
 end
 
-p cipher("What a string!", 5)
+p cipher('What a string!', 5)
